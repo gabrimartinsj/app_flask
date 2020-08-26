b20e62912897d35242d881e7dec529a56ab3811f
Gabrimartins <gabrimartinsjuarez@gmail.com>
2020-08-26 14:17:54 -0300

    Beta 1.5: Modal e dropdown funcionando.

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from flask import Flask, request, render_template # Importa a biblioteca
+import json
+from flask import Flask, request, render_template, jsonify   # Importa a biblioteca
 
 app = Flask(__name__) # Inicializa a aplicação
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template("index.html")
-
-@app.route('/run', methods=['GET', 'POST'])
-def run():
-    if request.method == 'POST':
-        if request.form['pass_value'] == 'Test':
-            value ='Test just for fools and horses!'
-            return render_template_string('''{{ value }}<br>''')
-        elif request.form['pass_value'] == 'Test2':
-            value ='Test2'
-            return render_template_string('''{{ value }}<br>''')
 
 if __name__ == '__main__':
     app.run(debug=True) # Executa a aplicação

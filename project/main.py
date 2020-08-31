8a53a8bc22c93b89b5e0c7e75de4d09a574115e7
Gabrimartins <gabrimartinsjuarez@gmail.com>
2020-08-31 13:16:22 -0300

    Beta 2.1: Telas integradas.

---
+++ project/main.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request
+from . import db
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/profile', methods=['POST', 'GET'])
+@login_required
+def profile():
+    if request.method == 'POST':
+        print(request.form['info3'])
+    return render_template('profile.html', name=current_user.name)
